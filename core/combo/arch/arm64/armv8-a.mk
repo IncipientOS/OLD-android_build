20168749e75ad3fc3232fb520a946236467cf02e
Alex Naidis <alex.naidis@linux.com>
2016-06-18 19:38:12 +0200

    core: combo: armv8-a: specific kryo tuning
    * cortex-a57 tuning is currently the best for kryo
    * The "kryo" tuning in clang, seems to be real bad
    
    Change-Id: I5c3bdeb931b98e786a7173b34a3dd416f14f7f96
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,25 +1,11 @@
-APPLY_A53_ERRATA_FIXES :=
-arch_variant_cflags :=
-
-ifneq ($(filter kryo,$(TARGET_2ND_CPU_VARIANT)),)
+ifneq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a57
-endif
-
-ifneq ($(filter cortex-a53 cortex-a53.a57,$(TARGET_2ND_CPU_VARIANT)),)
+else
+ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
-	APPLY_A53_ERRATA_FIXES := true
-endif
-
-ifneq ($(strip $(TARGET_IS_CORTEX-A53)),)
-	APPLY_A53_ERRATA_FIXES := $(TARGET_IS_CORTEX-A53)
-endif
-
-ifeq ($(APPLY_A53_ERRATA_FIXES),true)
-	arch_variant_cflags  += -mfix-cortex-a53-835769
-	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
-	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
 else
 	arch_variant_cflags :=
+endif
 endif
 
 ifneq (,$(filter cortex-a53 default,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
@@ -30,4 +16,3 @@ else
 	arch_variant_ldflags := -Wl,--no-fix-cortex-a53-843419
 	RS_DISABLE_A53_WORKAROUND := true
 endif
-
