de06132c382d0990173ef6f4fbd549f03454614d
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-04-11 07:24:08 +0200

    Target a53 for a53.a57. The a57 core re-oder the instructions as needed.

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,5 +1,23 @@
-ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+APPLY_A53_ERRATA_FIXES :=
+arch_variant_cflags :=
+
+ifneq ($(filter kryo,$(TARGET_2ND_CPU_VARIANT)),)
+	arch_variant_cflags := -mcpu=cortex-a57
+endif
+
+ifneq ($(filter cortex-a53 cortex-a53.a57,$(TARGET_2ND_CPU_VARIANT)),)
 	arch_variant_cflags := -mcpu=cortex-a53
+	APPLY_A53_ERRATA_FIXES := true
+endif
+
+ifneq ($(strip $(TARGET_IS_CORTEX-A53)),)
+	APPLY_A53_ERRATA_FIXES := $(TARGET_IS_CORTEX-A53)
+endif
+
+ifeq ($(APPLY_A53_ERRATA_FIXES),true)
+	arch_variant_cflags  += -mfix-cortex-a53-835769
+	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
+	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
 else
 	arch_variant_cflags :=
 endif
